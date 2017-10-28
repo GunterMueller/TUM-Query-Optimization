417c01b9c47fa1edff4a64fd84e1432f3fbbe176
Jonas Könning <jonaswk@users.noreply.github.com>
2017-10-28 11:30:05 +0200

    makefiles and examples

--- examples/ParserExample.cpp
+++ examples/ParserExample.cpp
@@ -1,8 +1,17 @@
+#include "Database.hpp"
+#include "Parser.hpp"
+#include <iostream>
 //---------------------------------------------------------------------------
 using namespace std;
 //---------------------------------------------------------------------------
 
 int main ()
 {
+    cout << "Parser Test \n";
+    Database db;
+    db.open("data/uni");
 
+    Parser parser;
+
+    parser.parse("SELECT s.MatrNr, s.Name, s.Semester FROM Studenten s, hoeren h, Vorlesungen v, Professoren p WHERE s.MatrNr=h.MatrNr AND h.VorlNr=v.VorlNr AND v.gelesenVon=p.PersNr AND p.Name=’Curie’");
 }
