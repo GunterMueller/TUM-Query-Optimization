f90e99a4c10bfde13ce2d43757e7b035caac2872
Jonas Könning <jonaswk@users.noreply.github.com>
2017-10-28 11:30:21 +0200

    New Query object

---
+++ src/Query.hpp
@@ -0,0 +1,17 @@
+#ifndef H_query
+#define H_query
+
+#include <list>
+#include <string>
+
+class Query {
+
+    public:
+    Query();
+    ~Query();
+    list<string> relationNames;
+    
+};
+
+
+#endif
