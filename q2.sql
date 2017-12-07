da992aac1a8f55a0a25039601d55a6d71fbef787
Jonas Könning <jonaswk@users.noreply.github.com>
2017-12-07 20:24:58 +0100

    Data

--- q2.sql
+++ q2.sql
@@ -1,3 +1,1 @@
-select s.name, s.semester
-from studenten s
-where 
+select * from lineitem l, orders o, customers c where l.l_orderkey=o.o_orderkey and o.o_custkey=c.c_custkey and c.c_name="Customer#000014993"
