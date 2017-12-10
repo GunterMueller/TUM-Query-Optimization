67db3e1cb960225549c5db601599b5e3293c23fb
Jonas Könning <jonaswk@users.noreply.github.com>
2017-12-10 10:05:14 +0100

    Makefile and first steps

--- src/DPAlgos.cpp
+++ src/DPAlgos.cpp
@@ -1,5 +1,22 @@
 #include "DPAlgos.hpp"
 
-static void DPsize() {
-    
+//Joins two trees
+static JoinTree CreateJoinTree(JoinTree&& left, JoinTree&& right) {
+    //We currently just use what is already implemented from previous exercises
+    //So no min. over join implementations
+    //Call the constructor
+    return JoinTree(std::move(left),std::move(right));
+}
+
+//In: A set of relations (here we start from parser result)
+//Out: Optimal bushy join tree
+static void DPsize(SQLParser::Result res) {
+
+    //Get set of relations
+    auto relations = res.relations;
+
+    for (auto& rel : relations) {
+        //Set entry in DP table
+    }
+
 }
--- src/DPAlgos.hpp
+++ src/DPAlgos.hpp
@@ -1,10 +1,14 @@
 #ifndef H_DPAlgos
 #define H_DPAlgos
 
+#include "JoinTree.hpp"
+#include "cts/parser/SQLParser.hpp"
+
 class DPAlgos {
 
     public:
     static void DPsize();
+    static void CreateJoinTree();
 
 };
 
