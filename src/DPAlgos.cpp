f1fa322b9b01de766853107a6c2462269a83acb5
Jonas Könning <jonaswk@users.noreply.github.com>
2017-12-10 09:32:48 +0100

    Stubs for algos

---
+++ src/DPAlgos.cpp
@@ -0,0 +1,5 @@
+#include "DPAlgos.hpp"
+
+static void DPsize() {
+    
+}
---
+++ src/DPAlgos.hpp
@@ -0,0 +1,11 @@
+#ifndef H_DPAlgos
+#define H_DPAlgos
+
+class DPAlgos {
+
+    public:
+    static void DPsize();
+
+};
+
+#endif
